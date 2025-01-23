4430f81fb5edc25a6251ca1ed742100fa362e8a5
PavloKostivUni <kostiv.pavlo@lll.kpi.ua>
2025-01-23 07:12:46 +0200

    property decorator

--- main.py
+++ main.py
@@ -3,50 +3,47 @@ from types import MethodType
 class Person:
   """Person class"""
 
-  gender = "Male"
+  _gender = "Male"
 
   def __init__(self, value, name):
     self.salary = value
     self.name = name
   
-  def get_name(self):
+  @property
+  def name(self):
+    """Person`s name property"""
     print("Name getter")
     return self._name
   
-  def set_name(self, value):
+  @name.setter
+  def name(self, value):
     print("Name setter")
     if isinstance(value, str) and len(value.strip()) > 0:
       self._name = value
     else:
       raise ValueError("Name must be a non-empty string")
   
-  def del_name(self):
+  @name.deleter
+  def name(self):
     print("Name deleter")
     del self._name
   
-  name = property(fget=get_name, fset=set_name, fdel=del_name, doc="Person`s name property")
+  gender = property(doc="Person`s gender property(set only)")
 
-  def say_hello(self):
-    print(f"Hello from {self.gender}")
-  
-  def register_method(self, func, name):
-    setattr(self, name, MethodType(func, self))
+  @gender.setter
+  def gender(self, gender):
+    print("Gender setter")
+    self._gender = gender
 
 
 p = Person(1000, "dimon")
-print(f"salary: {p.salary}")
-p.gender = "Female"
-
-
-def say_hello(self):
-  print(f"Hello from {self.gender} in a new function")
-
-p.register_method(say_hello, "sa_hello")
-
-Person.say_hello(Person)
-p.sa_hello()
-
 
 print(p.name)
 p.name = "pavlo"
 print(p.name)
+
+p.gender = "Female"
+
+print(p.__dict__)
+
+print(help(Person))
